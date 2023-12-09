c678a912515b86e33073f5866a55f56aa340a2c4
Davchick <davchick2000ort@gmail.com>
2023-12-09 20:30:28 +0300

    fix requests

--- server.js
+++ server.js
@@ -8,21 +8,16 @@ import cors from "cors";
 import path from "path";
 
 /* CONFIGURATIONS */
-const __dirname = path.resolve();
 const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../client/dist")));
 app.use(morgan("common"));
 dotenv.config();
 
 /* ROUTES */
 app.use("/auth", authRoutes);
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
-});
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 3000;
